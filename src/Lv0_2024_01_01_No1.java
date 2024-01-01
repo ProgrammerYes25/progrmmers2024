ec38ab65ff4a2a4001d1db00a0deffcb0aa6550a
ProgrammerYes25 <programmer.yes25@gmail.com>
2024-01-01 18:58:40 +0900

    2024.01.01 Lv0 //제목 : 문자열 출력하기 // //  문제 설명 //    문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요. // //  제한사항 //    1 ≤ str의 길이 ≤ 1,000,000 //    str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다. /// // 입출력 예 //  입력 #1 //    HelloWorld! // //  출력 #1 //    HelloWorld!

---
+++ src/Lv0_2024_01_01_No1.java
@@ -0,0 +1,25 @@
+//제목 : 문자열 출력하기
+//
+//  문제 설명
+//    문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+//
+//  제한사항
+//    1 ≤ str의 길이 ≤ 1,000,000
+//    str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다.
+///
+// 입출력 예
+//  입력 #1
+//    HelloWorld!
+//
+//  출력 #1
+//    HelloWorld!
+
+import java.util.Scanner;
+
+public class Lv0_2024_01_01_No1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str);
+    }
+}
