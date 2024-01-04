d6b01f87dd85a80b8a4766dd5f80c50d0cb6f8e6
ProgrammerYes25 <programmer.yes25@gmail.com>
2024-01-04 20:02:38 +0900

    2024.01.04 Lv0   No.4 //제목 : 더 크게 합치기 //  문제 설명 //    연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다. //    12 ⊕ 3 = 123 //    3 ⊕ 12 = 312 //    양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요. //    단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다. //  제한사항 //    1 ≤ a, b < 10,000 // //  입출력 예 //     a	b	result //     9	91	991 //    89	8	898 // 입출력 예 설명 //  입출력 예 #1 //    a ⊕ b = 991 이고, b ⊕ a = 919 입니다. 둘 중 더 큰 값은 991 이므로 991을 return 합니다. //  입출력 예 #2 //    a ⊕ b = 898 이고, b ⊕ a = 889 입니다. 둘 중 더 큰 값은 898 이므로 898을 return 합니다.

--- src/Lv0_2024_01_04_No3.java
+++ src/Lv0_2024_01_04_No3.java
@@ -1,39 +1,41 @@
-//제목 : 문자열 곱하기
+//제목 : 더 크게 합치기
 //  문제 설명
-//    문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+//    연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+//    12 ⊕ 3 = 123
+//    3 ⊕ 12 = 312
+//    양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+//    단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
 //  제한사항
-//    1 ≤ my_string의 길이 ≤ 100
-//    my_string은 영소문자로만 이루어져 있습니다.
-//    1 ≤ k ≤ 100
+//    1 ≤ a, b < 10,000
 //
 //  입출력 예
-//    my_string	  k	  result
-//     "string"	  3	  "stringstringstring"
-//      "love"	  10  "lovelovelovelovelovelovelovelovelovelove"
-//
+//     a	b	result
+//     9	91	991
+//    89	8	898
 // 입출력 예 설명
 //  입출력 예 #1
-//    예제 1번의 my_string은 "string"이고 이를 3번 반복한 문자열은 "stringstringstring"이므로 이를 return 합니다.
+//    a ⊕ b = 991 이고, b ⊕ a = 919 입니다. 둘 중 더 큰 값은 991 이므로 991을 return 합니다.
 //  입출력 예 #2
-//    예제 2번의 my_string은 "love"이고 이를 10번 반복한 문자열은 "lovelovelovelovelovelovelovelovelovelove"이므로 이를 return 합니다.
-
+//    a ⊕ b = 898 이고, b ⊕ a = 889 입니다. 둘 중 더 큰 값은 898 이므로 898을 return 합니다.
 
 import java.util.Scanner;
 
 public class Lv0_2024_01_04_No3 {
-    public String solution(String my_string, int k) {
-        String answer = my_string;
-        for(int i = 1; i < k; i++){
-            answer +=my_string;
-        }
+    public int solution(int a, int b) {
+        int answer = 0;
+        String tmp = a +""+b;
+        int i1 = (int)Integer.parseInt(tmp);
+        tmp = b+""+a;
+        int i2 = (int)Integer.parseInt(tmp);
+        answer = Math.max(i1, i2);
         return answer;
     }
     public static void main(String[] args){
         Lv0_2024_01_04_No3 s = new Lv0_2024_01_04_No3();
         Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        int k = sc.nextInt();
-        String str = s.solution(str1, k);
-        System.out.print(str);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int i = s.solution(a, b);
+        System.out.print(i);
     }
 }
