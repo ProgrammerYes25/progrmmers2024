eb1859b4792671a10649f9b5914c5c65c3091a7d
ProgrammerYes25 <programmer.yes25@gmail.com>
2024-01-12 16:57:47 +0900

    2024.01.12 Lv0   No.1 //제목 : 카운트 업 //  문제 설명 //    정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요. //  제한사항 //    0 ≤ start_num ≤ end_num ≤ 50 // //  입출력 예 //    start_num	end_num	result //        3	      10	[3, 4, 5, 6, 7, 8, 9, 10] // 입출력 예 설명 //  입출력 예 #1 //    3부터 10까지의 숫자들을 담은 리스트 [3, 4, 5, 6, 7, 8, 9, 10]를 return합니다.

---
+++ src/Lv0_2024_01_12_No1.java
@@ -0,0 +1,36 @@
+//제목 : 카운트 업
+//  문제 설명
+//    정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+//  제한사항
+//    0 ≤ start_num ≤ end_num ≤ 50
+//
+//  입출력 예
+//    start_num	end_num	result
+//        3	      10	[3, 4, 5, 6, 7, 8, 9, 10]
+// 입출력 예 설명
+//  입출력 예 #1
+//    3부터 10까지의 숫자들을 담은 리스트 [3, 4, 5, 6, 7, 8, 9, 10]를 return합니다.
+
+import java.util.Scanner;
+
+public class Lv0_2024_01_12_No1 {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer =  new int[end_num-start_num+1];
+
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = i+start_num;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Lv0_2024_01_12_No1 s = new Lv0_2024_01_12_No1();
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+        int[] arrans = s.solution(l, r);
+        for (int i : arrans) {
+            System.out.print(i + ",");
+        }
+    }
+}
