645b5a60820b3a17e1b0ad59fdbaf0f7b3968a3b
ProgrammerYes25 <programmer.yes25@gmail.com>
2024-02-14 11:36:29 +0900

    2024.02.14 Lv0   No.1 //제목 : 순서 바꾸기 //  문제 설명 //    정수 리스트 num_list와 정수 n이 주어질 때, num_list를 n 번째 원소 이후의 원소들과 n 번째까지의 원소들로 나눠 n 번째 원소 이후의 원소들을 n 번째까지의 원소들 앞에 붙인 리스트를 return하도록 solution 함수를 완성해주세요. //  제한사항 //    2 ≤ num_list의 길이 ≤ 30 //    1 ≤ num_list의 원소 ≤ 9 //    1 ≤ n ≤ num_list의 길이 //  입출력 예 //    num_list       	n	result //    [2, 1, 6]      	1	[1, 6, 2] //    [5, 2, 1, 7, 5]	3	[7, 5, 5, 2, 1] // // 입출력 예 설명 //  입출력 예 #1 //    [2, 1, 6]에서 첫 번째 이후의 원소는 [1, 6]이고 첫 번째까지의 원소는 [2]입니다. 두 리스트를 이어 붙이면 [1, 6, 2]가 됩니다. //  입출력 예 #2 //    [5, 2, 1, 7, 5]에서 세 번째 이후의 원소는 [7, 5]이고 세 번째까지의 원소는 [5, 2, 1]입니다. 두 리스트를 이어 붙이면 [7, 5, 5, 2, 1]가 됩니다.

--- src/Lv0_2024_02_14_No1.java
+++ src/Lv0_2024_02_14_No1.java
@@ -1,30 +1,33 @@
-//제목 : n 번째 원소부터
+//제목 : 순서 바꾸기
 //  문제 설명
-//    정수 리스트 num_list와 정수 n이 주어질 때, n 번째 원소부터 마지막 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+//    정수 리스트 num_list와 정수 n이 주어질 때, num_list를 n 번째 원소 이후의 원소들과 n 번째까지의 원소들로 나눠 n 번째 원소 이후의 원소들을 n 번째까지의 원소들 앞에 붙인 리스트를 return하도록 solution 함수를 완성해주세요.
 //  제한사항
 //    2 ≤ num_list의 길이 ≤ 30
 //    1 ≤ num_list의 원소 ≤ 9
 //    1 ≤ n ≤ num_list의 길이
 //  입출력 예
-//    num_list      	n	result
-//   [2, 1, 6]      	3	[6]
-//   [5, 2, 1, 7, 5]	2	[2, 1, 7, 5]
+//    num_list       	n	result
+//    [2, 1, 6]      	1	[1, 6, 2]
+//    [5, 2, 1, 7, 5]	3	[7, 5, 5, 2, 1]
 //
 // 입출력 예 설명
 //  입출력 예 #1
-//    [2, 1, 6]의 세 번째 원소부터 마지막 원소까지의 모든 원소는 [6]입니다.
+//    [2, 1, 6]에서 첫 번째 이후의 원소는 [1, 6]이고 첫 번째까지의 원소는 [2]입니다. 두 리스트를 이어 붙이면 [1, 6, 2]가 됩니다.
 //  입출력 예 #2
-//    [5, 2, 1, 7, 5]의 두 번째 원소부터 마지막 원소까지의 모든 원소는 [2, 1, 7, 5]입니다.
+//    [5, 2, 1, 7, 5]에서 세 번째 이후의 원소는 [7, 5]이고 세 번째까지의 원소는 [5, 2, 1]입니다. 두 리스트를 이어 붙이면 [7, 5, 5, 2, 1]가 됩니다.
 
 
 import java.util.Scanner;
 
 public class Lv0_2024_02_14_No1 {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length-n+1];
-        int index = 0;
-        for(int i = n-1; i < num_list.length; i++){
-            answer[index++] = num_list[i];
+        int[] answer = new int[num_list.length];
+        int j = 0;
+        for(int i = n; i < num_list.length; i++){
+            answer[j++] = num_list[i];
+        }
+        for(int i = 0; i < n; i++){
+            answer[j++] = num_list[i];
         }
         return answer;
     }
@@ -32,7 +35,7 @@ public class Lv0_2024_02_14_No1 {
     public static void main(String[] args) {
         Lv0_2024_02_14_No1 s = new Lv0_2024_02_14_No1();
         Scanner sc = new Scanner(System.in);
-        int[] i1 = {0, 1, 2, 3, 4, 5};
+        int[] i1 = {5, 2, 1, 7, 5};
         int i2 = sc.nextInt();
         int[] arrans = s.solution(i1, i2);
         for(int i : arrans){
